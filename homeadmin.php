cd31e45582f09e231831e050fe117356f59da3fd
farriagada <pinwy.bartok@gmail.com>
2014-07-12 04:25:40 -0400

    QUE WEA
    QUE WEA CONCHETUMARE

---
+++ homeadmin.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+<html>
+<?php
+session_start();
+?>
+<head>
+  <title>ADMIN SECTOR</title>
+  <meta name="description" content="website description" />
+  <meta name="keywords" content="website keywords, website keywords" />
+  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+  <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
+</head>
+
+<body>
+	<?php
+    include("DBconnection/connection.php");
+    ?>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.html">Reco<span class="logo_colour">Movie</span></a></h1>
+
+          <h2><?php
+   
+    echo "<h2>Bienvenido, " . $_SESSION['Nombre'] . "</h2>";
+    //echo "<br>";
+    print( '<h2><a href="edituser.php">Editar</a>' );
+    echo "- ";
+ 	print( '<a href="logout.php">Salir</a></br>' );
+    ?></h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li class="selected"><a href="home.php">Home</a></li>
+          <li><a href="catalogo.php">Cat&aacute;logo</a></li>
+          <li><a href="page.html">A Page</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="content_header"></div>
+    <div id="site_content">
+      <div class="sidebar">
+      	<h3>Search</h3>
+        <form method="post" action="#" id="search_form">
+          <p>
+            <input class="search" type="text" name="search_field" value="Enter keywords....." />
+            <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
+          </p>
+        </form>
+        <!-- insert your sidebar items here -->
+        <h3>WishList</h3>
+        <h4>New Website Launched</h4>
+        <h5>January 1st, 2010</h5>
+        <p>2010 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
+        <p></p>
+        <h4>New Website Launched</h4>
+        <h5>January 1st, 2010</h5>
+        <p>2010 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
+    
+        
+      </div>
+      <div id="content">
+        
+      </div>
+    </div>
+    <div id="content_footer"></div>
+    <div id="footer">
+    	RecoMovies es una Web dise&ntilde;ada para Ingenieria de Software, Universidad de Concepcion
+    </div>
+  </div>
+</body>
+</html>
