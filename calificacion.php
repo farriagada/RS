10c6ec3746db18cace1ddf8e121e75f68d3d5ac2
Diego Alonso Reyes Molina <diego2405@gmail.com>
2014-07-12 12:54:15 -0400

    nuevas funciones
    buscarCalificacion
    actualizar
    buscar
    
    cambio "formato" del get y el set

--- calificacion.php
+++ calificacion.php
@@ -1,13 +1,38 @@
 <?php
+include("pelicula.php");
+include("usuario.php");
 class Calificacion{
 	private $calificacion;
 	
-	public function __set($calificacion,$calificacionvalue){
+	public function setCalificacion($calificacionvalue){
 		return $this->$calificacion=$calificacionvalue;
 	}
 	
-	public function __get($calificacion){
+	public function getCalificacion(){
 		return $this->$calificacion;
-	}	
+	}
+	
+	public function buscarCalificacion($usuario, $pelicula){
+		$query=pg_query($dbconn,"SELECT nota FROM califica WHERE idu=$usuario->getId() and idp=$pelicula->getId()");
+		if(pg_num_rows($query)>0){
+			$res=pg_fetch_row($query);
+			$calificacion=$res[0];
+			return $this->$calificacion;
+		}else{
+			$calificacion=0;
+			return $this->$calificacion;
+		}
+	}
+	
+	public function actualizar($usuario,$pelicula,$calificacionV){
+		$this->$calificacion=$calificacionV;
+		pg_query($dbconn, "UPDATE califica SET nota=$calificacion 
+					   WHERE idu=$usuario->getId() and idp=$pelicula->getId()");
+	}
+	
+	public function crear($usuario,$pelicula,$calificacionV){
+		$this->$calificacion=$calificacionV;
+		$insert=pg_query($dbconn, "INSERT INTO califica(idu, idp, nota) VALUES ($usuario->getId(),$pelicula->getId(),$calificacion); ");
+	}
 }
 ?>
