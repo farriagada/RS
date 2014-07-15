cf9fe2d2897fc50905e30102554b020ebb96eeee
Diego Alonso Reyes Molina <diego2405@gmail.com>
2014-07-15 04:20:47 -0400

    Update director.php

--- director.php
+++ director.php
@@ -1,29 +1,30 @@
 <?php
 class Director{
+	
 	private $nombre;
-	private $apellido;
 	private $ano_nac;
-	public function __set($nombre,$nombrevalue){
-		return $this->$nombre=$nombrevalue;
+
+	public function setNombre($nombre){
+		return $this->nombre=$nombre;
 	}
 
-	public function __set($apellido,$apellidovalue){
-		return $this->$apellido=$apellidovalue;
-}	
+	public function setAnonac($ano_nac){
+		return $this->ano_nac=$ano_nac;
+	}	
 
-	public function __set($ano_nac,$ano_nacvalue){
-		return $this->$ano_nac=$ano_nacvalue;
-}	
-
-	public function __get($nombre){
-		return $this->$nombre;
-	}
-	public function __get($apellido){
-		return $this->$apellido;
+	public function getNombre(){
+		return $this->nombre;
 	}
 	
-	public function __get($ano_nac){
+	
+	public function getAnoNac($ano_nac){
 		return $this->$ano_nac;
+	}
+	
+	function retornarDirector($idpeli){
+        	$dbconn = pg_connect("host=localhost port=5432 dbname=Isw2 user=postgres password=lokoko20");
+        	$query = pg_query($dbconn, "SELECT nombre FROM dirije WHERE id=$idpeli");
+        	return $query;
 	}
 }
 ?>
